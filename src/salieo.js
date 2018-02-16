56bf8ac17e1e36e2760dd9cde659916634ae629f
Andrew Bagshaw <abagshaw@hotmail.ca>
2018-02-15 22:52:52 -0800

    Can't have ES6 :(

--- src/salieo.js
+++ src/salieo.js
@@ -11,8 +11,8 @@ function editMode(imgElements, cb) {
     document.body.appendChild(sheet);
 
     //Set up edit buttons on each image element
-    for(let i = 0; i < imgElements.length; i++) {
-        let editButton = document.createElement("a");
+    for(var i = 0; i < imgElements.length; i++) {
+        var editButton = document.createElement("a");
         editButton.innerHTML = "<svg class='edit-button' viewBox='0 0 24 24'><path d='M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z' /></svg>";
         editButton.className = "pre-entry"; //So that the edit button is added with transform: scale(0);
         (function(currentURL) {
